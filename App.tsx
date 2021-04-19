74c064c50bd036fbfc8bb9ebcbce0446ff114d04
UVNS.ShadePi <erikkirkegaardstage@gmail.com>
2021-04-19 16:58:16 +0200

    Background Colour

--- App.tsx
+++ App.tsx
@@ -56,40 +56,15 @@
  };
 
  const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
-
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
 
    return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
+     <View style={styles.background}>
+       <SafeAreaView style={styles.background}>
+        <Section title={'Custom Network Client'}>
+          A client for a custom network.
+        </Section>
+      </SafeAreaView>
+     </View>
    );
  };
 
@@ -109,6 +84,9 @@
    },
    highlight: {
      fontWeight: '700',
+   },
+   background: {
+     backgroundColor: '#246C96',
    },
 });
 
